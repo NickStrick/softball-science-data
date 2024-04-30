f4ef256dc807c70dfdf0cb328d7e249993dbae27
Nick Stricker <strickerdev@gmail.com>
2024-04-30 17:13:08 -0500

    cors update

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ require('dotenv').config()
 const cors = require('cors');
 const helmet = require('helmet');
 
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json())
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
     next();
   });
 
